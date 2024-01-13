5714227fa1aaaeba30ccb853cefd41c8541e609b
tonyArkaysia <tonyarkaysia@gmail.com>
2024-01-12 20:36:24 -0500

    app update

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask, request, session, redirect, url_for
 from flask_session import Session
-from flask_cors import CORS
+
 
 app = Flask(__name__)
-CORS(app) 
-
-
-app.secret_key = 'Gin632Jo'  # Set a secret key for security purposes
-app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
-
-def is_logged_in():
-    return session.get('logged_in')
+ 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,14 +24,6 @@ def login():
     # This part of the code executes for a GET request or when the form is initially loaded
     # It redirects to a retry login page since no login attempt has been made yet
     return redirect('https://tonyarkaysia.github.io/isl-retry-login') # Render a login template here
-
-@app.route('/add-api-key', methods=['POST'])
-def add_api_key():
-    apikey = request.form['apikey']
-    if apikey == "w-f_JoRyjxgqJKasc6glAQNFqFJIUGE7HwF_Lo0fQEA":  # Replace with real validation
-        return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
-    else:
-        return redirect('https://tonyarkaysia.github.io/isl-retry-login')
 
 
 if __name__ == '__main__':
