82adc032797c6e9059ba6d3190a896628cae7900
tonyArkaysia <tonyarkaysia@gmail.com>
2024-01-13 13:56:09 -0500

    app update

--- testapp.py
+++ testapp.py
@@ -2,9 +2,9 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/tester')
+@app.route('/tester', methods=['POST'])
 def tester():
     return redirect('https://tonyarkaysia.github.io/isl-cybergateway')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
